616869ae384a19e38446fc2d3730e99ee4dd2522
Marcoscappelli <@rmagedon2016>
2022-10-06 09:09:40 -0300

    Criando Message

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -30,7 +30,7 @@ class EventController extends Controller
 
         $event->save();
 
-        return redirect('/');
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
     }
 
     public function login() {
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -50,7 +50,16 @@
             </nav>    
 
         </header>
-       @yield('content')
+        <main>
+            <div class="container-fluid">
+                <div class="row">
+                @if(session('msg'))
+                    <p class="msg">{{ session('msg') }}</p>
+                @endif    
+                @yield('content')
+                </div>
+            </div>
+        </main>
         <footer>
             <p>MVC Events &copy; 2022</p>
         </footer>
