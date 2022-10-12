4787ad6aeaa98ffa1c8de7b3faba01c748782512
Marcoscappelli <@rmagedon2016>
2022-10-11 21:48:45 -0300

    criando session

--- database/migrations/2022_10_12_000502_add_date_to_events_table.php
+++ database/migrations/2022_10_12_000502_add_date_to_events_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->dateTime('date');
+           $table->dateTime('date');
         });
     }
 
